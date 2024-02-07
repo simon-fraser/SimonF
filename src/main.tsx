5922e512e42dd19debe160ed9fe638596e387903
Simon Fraser <simon.fraser@insurwave.com>
2024-02-07 08:35:11 +0000

    structure updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Splash from './components/splash'
-import Links from './components/links'
 
 import './main.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Splash />
-    <Links />
-    <div className='relative z-10'>
-      <App />
-    </div>
+    <App />
   </React.StrictMode>,
 )
